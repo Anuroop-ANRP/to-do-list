7d96e29931b133691db61da94805e0fdaa1cb6f3
ANRP <anuroopanrp@gmail.com>
2024-08-08 16:54:32 +0530

    First Commit to the TO DO List app

---
+++ src/main/java/org/anrp/Main.java
@@ -0,0 +1,13 @@
+package org.anrp;
+
+public class Main {
+
+    static {
+        System.out.println("Welcome to the TO DO List");
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
